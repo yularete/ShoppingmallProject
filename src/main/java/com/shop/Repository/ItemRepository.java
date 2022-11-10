e29df4e22693cdcd688fac47cc5211ee522336a6
yularete <keinikeiwo@gmail.com>
2022-11-10 22:34:23 +0900

    feat : @Query를 이용한 검색 처리

--- src/main/java/com/shop/Repository/ItemRepository.java
+++ src/main/java/com/shop/Repository/ItemRepository.java
@@ -2,6 +2,8 @@ package com.shop.Repository;
 
 import com.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -15,4 +17,6 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
     List<Item> findByPriceLessThan(Integer price);
     List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
 }
--- src/test/java/com/shop/Repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/Repository/ItemRepositoryTest.java
@@ -20,20 +20,20 @@ class ItemRepositoryTest {
     @Autowired //ItemRepository를 사용하기 위해 Autowired 어노테이션을 이용해 Bean을 주입.
     ItemRepository itemRepository;
 
-//    @Test
-//    @DisplayName("상품 저장 테스트")
-//    public void createItemTest(){
-//        Item item = new Item();
-//        item.setItemNm("테스트 상품");
-//        item.setPrice(10000);
-//        item.setItemDetail("테스트 상품 상세 설명");
-//        item.setItemSellStatus(ItemSellStatus.SELL);
-//        item.setStockNumber(100);
-//        item.setRegTime(LocalDateTime.now());
-//        item.setUpdateTime(LocalDateTime.now());
-//        Item savedItem = itemRepository.save(item);
-//        System.out.println(savedItem.toString());
-//    }
+    @Test
+    @DisplayName("상품 저장 테스트")
+    public void createItemTest(){
+        Item item = new Item();
+        item.setItemNm("테스트 상품");
+        item.setPrice(10000);
+        item.setItemDetail("테스트 상품 상세 설명");
+        item.setItemSellStatus(ItemSellStatus.SELL);
+        item.setStockNumber(100);
+        item.setRegTime(LocalDateTime.now());
+        item.setUpdateTime(LocalDateTime.now());
+        Item savedItem = itemRepository.save(item);
+        System.out.println(savedItem.toString());
+    }
 
     public void createItemList(){
         for(int i=1;i<=10;i++){
@@ -48,36 +48,36 @@ class ItemRepositoryTest {
             Item savedItem = itemRepository.save(item);
         }
     }
-//
-//    @Test
-//    @DisplayName("상품명 조회 테스트")
-//    public void findByItemNmTest(){
-//        this.createItemList(); //테스트 상품을 만드는 메소드를 실행하여 조회할 대상을 만들어 줌 !
-//        List<Item> itemList = itemRepository.findByItemNm("테스트 상품1"); //메소드 호출 뒤 파라미터로 "테스트 상품1" 상품명 전달
-//        for(Item item : itemList){
-//            System.out.println(item.toString());
-//        }
-//    }
 
-//    @Test
-//    @DisplayName("상품명, 상품상세설명 or 테스트")
-//    public void findByItemNmOrItemDetailTest(){
-//        this.createItemList();
-//        List<Item> itemList = itemRepository.findByItemNmOrItemDetail("테스트 상품1","테스트 상품 상세 설명5");
-//        for(Item item : itemList){ // item의 내용을 itemList에 할당해주는?
-//            System.out.println(item.toString());
-//        }
-//    }
+    @Test
+    @DisplayName("상품명 조회 테스트")
+    public void findByItemNmTest(){
+        this.createItemList(); //테스트 상품을 만드는 메소드를 실행하여 조회할 대상을 만들어 줌 !
+        List<Item> itemList = itemRepository.findByItemNm("테스트 상품1"); //메소드 호출 뒤 파라미터로 "테스트 상품1" 상품명 전달
+        for(Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
 
-//    @Test
-//    @DisplayName("가격 LessThan 테스트")
-//    public void findByPriceLessThanTest(){
-//        this.createItemList();
-//        List<Item> itemList = itemRepository.findByPriceLessThan(10005);
-//        for (Item item : itemList){
-//            System.out.println(item.toString());
-//        }
-//    }
+    @Test
+    @DisplayName("상품명, 상품상세설명 or 테스트")
+    public void findByItemNmOrItemDetailTest(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemNmOrItemDetail("테스트 상품1","테스트 상품 상세 설명5");
+        for(Item item : itemList){ // item의 내용을 itemList에 할당해주는?
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("가격 LessThan 테스트")
+    public void findByPriceLessThanTest(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByPriceLessThan(10005);
+        for (Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
 
     @Test
     @DisplayName("가격 내림차순 조회 테스트")
@@ -85,6 +85,16 @@ class ItemRepositoryTest {
         this.createItemList();
         List<Item> itemList = itemRepository.findByPriceLessThanOrderByPriceDesc(10005);
         for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("@Query를 이용한 상품 조회 테스트")
+    public void findByItemDetailTest(){
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemDetail("테스트 상품 상세 설명");
+        for(Item item : itemList){
             System.out.println(item.toString());
         }
     }
