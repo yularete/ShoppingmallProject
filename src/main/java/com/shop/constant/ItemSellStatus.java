04bd5fd9551f958d5a4cd883faad5c1fa6c1ac11
yularete <keinikeiwo@gmail.com>
2022-11-10 17:01:56 +0900

    feat : 상품 엔티티 설계  (java 버전 관리는 pom.xml에서 관리하시면 됩니다...)

---
+++ src/main/java/com/shop/constant/ItemSellStatus.java
@@ -0,0 +1,6 @@
+package com.shop.constant;
+
+//enum 타입의 class를 사용해 연관된 상수들을 모아둘 수 있으며, enum에 정의한 타입만 값을 가지도록 컴파일 시 체크를 할 수 있다.
+public enum ItemSellStatus {
+    SELL, SOLD_OUT
+}
---
+++ src/main/java/com/shop/entity/Item.java
@@ -0,0 +1,43 @@
+package com.shop.entity;
+
+import com.shop.constant.ItemSellStatus;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="item")
+@Getter
+@Setter
+@ToString
+public class Item {
+//실제론 여러가지 옵션 및 옵션 상품의 가격, 재고, 배송 방법에 대한 정보도 담겨있는데 최대한 간단하게 !
+
+    @Id
+    @Column(name="item_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;      //상품코드
+
+    @Column(nullable = false, length=50)
+    private String itemNm;     //상품명
+
+    @Column(name = "price", nullable = false)
+    private int price;         //가격
+
+    @Column(nullable = false)
+    private int stockNumber;      //재고수량
+
+    @Lob
+    @Column(nullable = false)
+    private String itemDetail;    //상품 상세 설명
+
+    @Enumerated(EnumType.STRING)
+    private ItemSellStatus itemSellStatus;   //상품 판매 상태
+
+    private LocalDateTime regTime;   // 등록 시간
+
+    private LocalDateTime updateTime;   // 수정 시간
+}
