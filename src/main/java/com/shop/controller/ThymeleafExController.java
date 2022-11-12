6bc507f66b4018f472dd0c8f38b282e4c6142d3e
yularete <keinikeiwo@gmail.com>
2022-11-12 23:14:45 +0900

    feat: th:each를 이용해 like for문같은 반복문 사용

--- src/main/java/com/shop/controller/ThymeleafExController.java
+++ src/main/java/com/shop/controller/ThymeleafExController.java
@@ -1,12 +1,15 @@
 package com.shop.controller;
 
 import com.shop.dto.ItemDto;
+import com.shop.entity.Item;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping(value="/thymeleaf") //url에 /thymeleaf경로로 오는 요청을 이 컨트롤러가 하겠다 !
@@ -30,5 +33,24 @@ public class ThymeleafExController {
 
         model.addAttribute("itemDto", itemDto);
         return "thymeleafEx/thymeleafEx02";
+    }
+
+    @GetMapping(value = "/ex03")
+    public String thymeleafExample03(Model model) {
+
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        for (int i = 1; i <= 10; i++) {
+
+            ItemDto itemDto = new ItemDto();
+            itemDto.setItemDetail("상품 상세 설명" + i);
+            itemDto.setItemNm("테스트 상품" + i);
+            itemDto.setPrice(10000 + i);
+            itemDto.setRegTime(LocalDateTime.now());
+
+            itemDtoList.add(itemDto);
+        }
+        model.addAttribute("itemDtoList", itemDtoList);
+        return "thymeleafEx/thymeleafEx03";
     }
 }
