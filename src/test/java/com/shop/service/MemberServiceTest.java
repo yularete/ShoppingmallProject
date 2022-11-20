ca6ab4a10e21833e5d25877a6d4cb3209af55825
yularete <keinikeiwo@gmail.com>
2022-11-20 23:58:45 +0900

    test : 회원가입 테스트 수정

--- src/test/java/com/shop/service/MemberServiceTest.java
+++ src/test/java/com/shop/service/MemberServiceTest.java
@@ -2,6 +2,8 @@ package com.shop.service;
 
 import com.shop.dto.MemberFormDto;
 import com.shop.entity.Member;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -12,7 +14,7 @@ import javax.transaction.Transactional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@Transactional
+@Transactional //테스트 실행 후 롤백 처리가 됨! 반복적인 테스트 가능ㅎㅎ
 @TestPropertySource(locations = "classpath:application-test.properties")
 class MemberServiceTest {
 
@@ -22,13 +24,26 @@ class MemberServiceTest {
     @Autowired
     PasswordEncoder passwordEncoder;
 
-    public Member createMember(){
+    public Member createMember(){ //회원정보를 입력한 Member Entity를 만듦
         MemberFormDto memberFormDto = new MemberFormDto();
         memberFormDto.setEmail("test@email.com");
         memberFormDto.setName("홍길동");
         memberFormDto.setAddress("서울시 마포구 합정동");
         memberFormDto.setPassword("1234");
         return Member.createMember(memberFormDto, passwordEncoder);
+    }
+
+    @Test
+    @DisplayName("회원가입 테스트")
+    public void saveMemberTest(){ //저장하려고 요청한 값과 실제 저장된 데이터를 비교! 첫째는 기대 값, 둘째는 실제로 저장된 값을 파라미터로 넣어쥼.
+        Member member = createMember();
+        Member savedMember = memberService.saveMember(member);
+
+        assertEquals(member.getEmail(), savedMember.getEmail());
+        assertEquals(member.getName(), savedMember.getName());
+        assertEquals(member.getAdress(),savedMember.getAdress());
+        assertEquals(member.getPassword(),savedMember.getPassword());
+        assertEquals(member.getRole(),savedMember.getRole());
     }
 
 }
