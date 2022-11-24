375ee6bdaffb94a3fd38c83026cc4bbd164817a3
yularete <keinikeiwo@gmail.com>
2022-11-24 22:46:55 +0900

    feat : 회원가입시 여러 검증을 위한 controller method 작성

--- src/main/java/com/shop/controller/MemberController.java
+++ src/main/java/com/shop/controller/MemberController.java
@@ -7,9 +7,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @RequestMapping("/members")
 @Controller
@@ -30,6 +33,23 @@ public class MemberController {
         Member member = Member.createMember(memberFormDto, passwordEncoder);
         memberService.saveMember(member);
 
+        return "redirect:/";
+    }
+    @PostMapping(value = "/new")
+    public String newMember(@Valid MemberFormDto memberFormDto, BindingResult bindingResult, Model model){
+        //검증하려는 객체의 앞에 @Vailid 어노테이션을 선언, 파라미터로 bindingResult 객체 추가.
+        //검사 후 결과는 bindingResult에 담아준다.
+        //bindingResult.hasErrors()를 호출하여 에러가 있다면 회원 가입 페이지로 이동.
+        if(bindingResult.hasErrors()){
+            return "member/memberForm";
+        }
+        try{
+            Member member = Member.createMember(memberFormDto, passwordEncoder);
+            memberService.saveMember(member);
+        } catch (IllegalStateException e){
+            model.addAttribute("errorMessage",e.getMessage()); //중복 회원 가입 예외 발생시 에러메시지 뷰로 전달.
+            return "member/memberForm";
+        }
         return "redirect:/";
     }
 }
