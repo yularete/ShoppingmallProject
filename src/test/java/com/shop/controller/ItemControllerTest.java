672cb78917b404c3814414979d82ba60106adf07
yularete <keinikeiwo@gmail.com>
2022-11-26 20:50:52 +0900

    test: 상품 등록 페이지 권한 테스트

---
+++ src/test/java/com/shop/controller/ItemControllerTest.java
@@ -0,0 +1,34 @@
+package com.shop.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(locations="classpath:application-test.properties")
+class ItemControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @DisplayName("상품 등록 페이지 권한 테스트")
+    @WithMockUser(username = "admin", roles = "ADMIN")
+    public void itemFromTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/admin/item/new"))//상품 등록 페이지에 get요청을 보낸다.
+                .andDo(print()) // 요청과 응답 메시지를 확인할 수 있도록 콘솔창에 출력해준다.
+                .andExpect(status().isOk()); //응답 상태 코드가 정상인지 확인
+    }
+
+}
