a9327edb6b8fac549c16cff470147a715ff3a5dd
yularete <keinikeiwo@gmail.com>
2022-12-04 23:45:01 +0900

    test : 회원 엔티티 저장 시 자동으로 등록자, 수정자, 등록시간, 수정시간이 저장되는지 membertest

---
+++ src/test/java/com/shop/entity/MemberTest.java
@@ -0,0 +1,48 @@
+package com.shop.entity;
+
+import com.shop.Repository.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.TestPropertySource;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@TestPropertySource(locations="classpath:application-test.properties")
+class MemberTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Test
+    @DisplayName("Auditing 테스트")
+    @WithMockUser(username = "gildong", roles = "USER") //로그인 한 상태라 가정하고 테스트 진행 가능
+    public void auditingTest(){
+        Member newMember = new Member();
+        memberRepository.save(newMember);
+
+        em.flush();
+        em.clear();
+
+        Member member = memberRepository.findById(newMember.getId())
+                .orElseThrow(EntityNotFoundException::new);
+
+        System.out.println("resister time : " + member.getRegTime());
+        System.out.println("update time : " + member.getUpdateTime());
+        System.out.println("clear member : " + member.getCreatedBy());
+        System.out.println("modify member : " + member.getModifiedBy());
+    }
+}
