eff81a6e0d4cdcf468c43b20c4f16b0690afd186
yularete <keinikeiwo@gmail.com>
2022-12-06 22:18:38 +0900

    feat : 상품 이미지를 정보를 저장하는 itemImgService작성

---
+++ src/main/java/com/shop/service/ItemImgService.java
@@ -0,0 +1,33 @@
+package com.shop.service;
+
+import com.shop.Repository.ItemImgRepository;
+import com.shop.entity.ItemImg;
+import lombok.Value;
+import org.springframework.web.multipart.MultipartFile;
+import org.thymeleaf.util.StringUtils;
+
+public class ItemImgService {
+
+    @Value("${itemImgLocation}")
+    private String itemImgLocation;
+
+    private final ItemImgRepository itemImgRepository;
+
+    private final FileService fileService;
+
+    public void saveItemImg(ItemImg itemImg, MultipartFile itemImgFile) throws Exception {
+        String oriImgName = itemImgFile.getOriginalFilename();
+        String imgName = "";
+        String imgUrl = "";
+
+        //파일 업로드
+        if(!StringUtils.isEmpty(oriImgName)){
+            imgName = fileService.uploadFile(itemImgLocation, oriImgName, itemImgFile.getBytes());
+            imgUrl = "/images/item/" + imgName;
+        }
+
+        //상품 이미지 정보 저장
+        itemImg.updateItemImg(oriImgName, imgName, imgUrl);
+        itemImgRepository.save(itemImg);
+    }
+}
