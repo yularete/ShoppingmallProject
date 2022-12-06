030e40a43df7614cd9fea7c97871143fd758c6c0
yularete <keinikeiwo@gmail.com>
2022-12-06 22:14:15 +0900

    feat : 상품 이미지 정보를 저장하기 위한 ItemImgRepository생성

---
+++ src/main/java/com/shop/Repository/ItemImgRepository.java
@@ -0,0 +1,7 @@
+package com.shop.Repository;
+
+import com.shop.entity.ItemImg;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
+}
