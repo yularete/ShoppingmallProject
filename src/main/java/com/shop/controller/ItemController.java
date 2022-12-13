62ff117bdbf89f4164cb02c5773b415a4c7e76cc
yularete <keinikeiwo@gmail.com>
2022-12-13 23:43:14 +0900

    refactoring : itemDtl 메소드 수정

--- src/main/java/com/shop/controller/ItemController.java
+++ src/main/java/com/shop/controller/ItemController.java
@@ -49,17 +49,25 @@ public class ItemController {
         return "redirect:/";
     }
 
-    @GetMapping(value = "/admin/item/{itemId}")
+//    @GetMapping(value = "/admin/item/{itemId}")
+//    public String itemDtl(@PathVariable("itemId") Long itemId, Model model){
+//        try{
+//            ItemFormDto itemFormDto =itemService.getItemDtl(itemId);
+//            model.addAttribute("itemFormDto", itemFormDto);
+//        } catch (EntityNotFoundException e){
+//            model.addAttribute("errorMessage", "존재하지 않는 상품 입니다.");
+//            model.addAttribute("itemFormDto", new ItemFormDto());
+//            return "item/itemForm";
+//        }
+//        return "item/itemForm";
+//    }
+
+    @GetMapping(value = "/item/{itemId}")
     public String itemDtl(@PathVariable("itemId") Long itemId, Model model){
-        try{
+
             ItemFormDto itemFormDto =itemService.getItemDtl(itemId);
-            model.addAttribute("itemFormDto", itemFormDto);
-        } catch (EntityNotFoundException e){
-            model.addAttribute("errorMessage", "존재하지 않는 상품 입니다.");
-            model.addAttribute("itemFormDto", new ItemFormDto());
-            return "item/itemForm";
-        }
-        return "item/itemForm";
+            model.addAttribute("item", itemFormDto);
+        return "item/itemDtl";
     }
 
     @PostMapping(value = "/admin/item/{itemId}")
